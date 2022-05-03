2c2cf6e3dd1000b1baf0dc6b68cb2206bb2a7f3d
Tien Phuc <leesinvn123@gmail.com>
2022-05-03 22:33:58 +0700

    Implement the header for the page

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -26,6 +26,7 @@ import about03 from "../assets/about03.png";
 import about04 from "../assets/about04.png";
 
 import profile from "../assets/profile.png";
+import avatar from "../assets/avatar.png";
 import circle from "../assets/circle.svg";
 import logo from "../assets/logo.png";
 
@@ -73,4 +74,5 @@ export default {
   spotify,
   nextjs,
   antd,
+  avatar,
 };
