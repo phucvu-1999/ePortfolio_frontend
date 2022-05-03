6f4859d5b6c65577545f318aef85193c4fbb55fc
Tien Phuc <leesinvn123@gmail.com>
2022-05-03 12:29:30 +0700

    Implement file structure for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import { About, Footer, Header, Skills, Testimonial, Works } from "./container";
+import { Navbar } from "./components";
+
+import "./App.scss";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className="app">
+      <Header />
+      <About />
+      <Works />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
